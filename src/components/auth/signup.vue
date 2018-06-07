9797464e9b7e4057c97dd8ec89c91726749f37af
Charlie Frye <32540979+LeMentore@users.noreply.github.com>
2018-06-07 19:26:34 +0500

    added ui handlers for validator

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -2,13 +2,15 @@
     <div id="signup">
         <div class="signup-form">
             <form @submit.prevent="onSubmit">
-                <div class="input">
+                <div class="input" :class="{invalid: $v.email.$error}">
                     <label for="email">Ящик</label>
                     <input
                         type="email"
                         id="email"
                         @input="$v.email.$touch()"
                         v-model="email">
+                    <p v-if="!$v.email.email">Некорректный ящик</p>
+                    <p v-if="!$v.email.required">Обяательное поле</p>
                 </div>
                 <div class="input">
                     <label for="age">Возраст</label>
@@ -157,6 +159,15 @@
         outline: none;
         border: 1px solid #521751;
         background-color: #eee;
+    }
+
+    .input.invalid input {
+        border: 1px solid red;
+        background-color: #ffc9aa;
+    }
+
+    .input.invalid label {
+        color: red
     }
 
     .input select {
